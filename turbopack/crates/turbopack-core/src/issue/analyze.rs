8fe8558802060759daad6d1b5e6d0eee94a01cec
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-01 15:03:46 -0700

    Fix turbopack-core

--- turbopack/crates/turbopack-core/src/issue/analyze.rs
+++ turbopack/crates/turbopack-core/src/issue/analyze.rs
@@ -70,7 +70,7 @@ impl Issue for AnalyzeIssue {
     }
 
     #[turbo_tasks::function]
-    fn file_path(&self) -> FileSystemPath {
+    fn file_path(&self) -> Vc<FileSystemPath> {
         self.source_ident.path()
     }
 
