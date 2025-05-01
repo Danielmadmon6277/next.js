1631ff63ddfa9eb4cfe0800afc917944268ee03e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-01 15:02:30 -0700

    Fix turbopack-core

--- turbopack/crates/turbopack-core/src/issue/code_gen.rs
+++ turbopack/crates/turbopack-core/src/issue/code_gen.rs
@@ -29,7 +29,7 @@ impl Issue for CodeGenerationIssue {
     }
 
     #[turbo_tasks::function]
-    fn file_path(&self) -> FileSystemPath {
+    fn file_path(&self) -> Vc<FileSystemPath> {
         *self.path
     }
 
