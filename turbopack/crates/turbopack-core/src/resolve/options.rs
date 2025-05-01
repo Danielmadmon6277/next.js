802c376f96fe9d72403667c7f91ce71e7a6e851d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-01 16:01:11 -0700

    Fix turbopack-core

--- turbopack/crates/turbopack-core/src/resolve/options.rs
+++ turbopack/crates/turbopack-core/src/resolve/options.rs
@@ -470,7 +470,7 @@ impl ValueToString for ImportMapResult {
             ImportMapResult::AliasExternal { .. } => Ok(Vc::cell("TODO external".into())),
             ImportMapResult::Alias(request, context) => {
                 let s = if let Some(path) = context {
-                    let path = path.to_string().await?;
+                    let path = path.to_string();
                     format!(
                         "aliased to {} inside of {}",
                         request.to_string().await?,
