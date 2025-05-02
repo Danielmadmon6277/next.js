c52311c4d909c4a03270afba97972fc828bcd8b8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-02 08:14:03 -0700

    iter()

--- turbopack/crates/turbo-tasks-backend/src/kv_backing_storage.rs
+++ turbopack/crates/turbo-tasks-backend/src/kv_backing_storage.rs
@@ -722,7 +722,7 @@ fn save_strings_serial<'a>(
     strings: &RcStrToLocalId,
 ) -> Result<LocalIdToGlobalId> {
     let mut global_ids = Vec::new();
-    for s in strings.0.iter() {
+    for s in strings.iter() {
         let (global_id, is_new) = get_string_id(batch, s)?;
         if is_new {
             batch.put(
@@ -748,7 +748,7 @@ fn save_strings_concurrent<'a>(
     strings: &RcStrToLocalId,
 ) -> Result<LocalIdToGlobalId> {
     let mut global_ids = Vec::new();
-    for s in strings.0.iter() {
+    for s in strings.iter() {
         let (global_id, is_new) = get_string_id(batch, s)?;
         if is_new {
             batch.put(
