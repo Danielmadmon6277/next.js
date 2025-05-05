49e6582c6288935aae8e187f83936ec514c98cda
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 09:08:00 -0700

    next-core

--- crates/next-core/src/next_client/runtime_entry.rs
+++ crates/next-core/src/next_client/runtime_entry.rs
@@ -33,7 +33,7 @@ impl RuntimeEntry {
         };
 
         let modules = cjs_resolve(
-            Vc::upcast(PlainResolveOrigin::new(asset_context, *path)),
+            Vc::upcast(PlainResolveOrigin::new(asset_context, path.clone())),
             *request,
             None,
             false,
