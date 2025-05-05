74c542841c5e3a7d7f8adc5702de9f7709fe12d0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 10:26:37 -0700

    next-core

--- crates/next-core/src/next_app/app_client_references_chunks.rs
+++ crates/next-core/src/next_app/app_client_references_chunks.rs
@@ -192,8 +192,8 @@ pub async fn get_app_client_references_chunks(
                 let base_ident = server_component.ident();
 
                 let server_path = server_component.server_path().await?;
-                let is_layout = server_path.file_stem().await?.as_deref() == Some("layout");
-                let server_component_path = server_path.to_string().await?;
+                let is_layout = server_path.file_stem().as_deref() == Some("layout");
+                let server_component_path = server_path.to_string();
 
                 let ssr_modules = client_reference_types
                     .iter()
