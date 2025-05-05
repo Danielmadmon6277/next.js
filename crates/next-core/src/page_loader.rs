663d4ba624db49fedb4ce355bc98b67a5d27e0cf
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 11:50:23 -0700

    next-core

--- crates/next-core/src/page_loader.rs
+++ crates/next-core/src/page_loader.rs
@@ -34,7 +34,7 @@ pub async fn create_page_loader_entry_module(
         StringifyJs(&*pathname.await?)
     )?;
 
-    let page_loader_path = next_js_file_path("entry/page-loader.ts".into());
+    let page_loader_path = next_js_file_path("entry/page-loader.ts".into()).await?;
     let base_code = page_loader_path.read();
     if let FileContent::Content(base_file) = &*base_code.await? {
         result += base_file.content()
@@ -145,7 +145,7 @@ impl OutputAsset for PageLoaderAsset {
         let root = self
             .rebase_prefix_path
             .await?
-            .map_or(self.server_root.clone(), |path| *path);
+            .map_or(self.server_root.clone(), |path| path);
         Ok(root.join(
             format!(
                 "static/chunks/pages{}",
