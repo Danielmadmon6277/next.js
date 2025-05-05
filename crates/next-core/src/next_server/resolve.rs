886be2cd1e4f4edb5803ac923375101dfe64e987
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 10:30:12 -0700

    next-core

--- crates/next-core/src/next_server/resolve.rs
+++ crates/next-core/src/next_server/resolve.rs
@@ -480,8 +480,8 @@ impl Issue for ExternalizeIssue {
     }
 
     #[turbo_tasks::function]
-    fn file_path(&self) -> FileSystemPath {
-        *self.file_path
+    fn file_path(&self) -> Vc<FileSystemPath> {
+        self.file_path.clone().cell()
     }
 
     #[turbo_tasks::function]
