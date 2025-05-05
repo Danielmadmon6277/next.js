1d8df71ea2517a8aa04302028c48bf31f185b62f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 12:13:17 -0700

    next-core

--- crates/next-core/src/next_client/context.rs
+++ crates/next-core/src/next_client/context.rs
@@ -2,7 +2,7 @@ use std::iter::once;
 
 use anyhow::Result;
 use turbo_rcstr::RcStr;
-use turbo_tasks::{FxIndexMap, OptionVcExt, ResolvedVc, Value, Vc};
+use turbo_tasks::{FxIndexMap, ResolvedVc, Value, Vc};
 use turbo_tasks_env::EnvMap;
 use turbo_tasks_fs::FileSystemPath;
 use turbopack::{
