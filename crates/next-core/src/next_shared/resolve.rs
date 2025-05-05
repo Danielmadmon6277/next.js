319170ab589d4c4cbe4cedac0b6217d2783ea41e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 11:49:07 -0700

    next-core

--- crates/next-core/src/next_shared/resolve.rs
+++ crates/next-core/src/next_shared/resolve.rs
@@ -153,7 +153,7 @@ pub(crate) fn get_invalid_client_only_resolve_plugin(
     root: FileSystemPath,
 ) -> Vc<InvalidImportResolvePlugin> {
     InvalidImportResolvePlugin::new(
-        *root,
+        root,
         "client-only".into(),
         vec![
             "'client-only' cannot be imported from a Server Component module. It should only be \
@@ -170,7 +170,7 @@ pub(crate) fn get_invalid_server_only_resolve_plugin(
     root: FileSystemPath,
 ) -> Vc<InvalidImportResolvePlugin> {
     InvalidImportResolvePlugin::new(
-        *root,
+        root,
         "server-only".into(),
         vec![
             "'server-only' cannot be imported from a Client Component module. It should only be \
@@ -185,7 +185,7 @@ pub(crate) fn get_invalid_styled_jsx_resolve_plugin(
     root: FileSystemPath,
 ) -> Vc<InvalidImportResolvePlugin> {
     InvalidImportResolvePlugin::new(
-        *root,
+        root,
         "styled-jsx".into(),
         vec![
             "'client-only' cannot be imported from a Server Component module. It should only be \
@@ -215,11 +215,11 @@ impl NextExternalResolvePlugin {
 #[turbo_tasks::value_impl]
 impl AfterResolvePlugin for NextExternalResolvePlugin {
     #[turbo_tasks::function]
-    fn after_resolve_condition(&self) -> Vc<AfterResolvePluginCondition> {
-        AfterResolvePluginCondition::new(
-            self.project_path.root(),
+    async fn after_resolve_condition(&self) -> Result<Vc<AfterResolvePluginCondition>> {
+        Ok(AfterResolvePluginCondition::new(
+            (*self.project_path.root().await?).clone(),
             Glob::new("**/next/dist/**/*.{external,runtime.dev,runtime.prod}.js".into()),
-        )
+        ))
     }
 
     #[turbo_tasks::function]
@@ -230,7 +230,7 @@ impl AfterResolvePlugin for NextExternalResolvePlugin {
         _reference_type: Value<ReferenceType>,
         _request: Vc<Request>,
     ) -> Result<Vc<ResolveResultOption>> {
-        let path = fs_path.await?.path.to_string();
+        let path = fs_path.path.to_string();
         // Find the starting index of 'next/dist' and slice from that point. It should
         // always be found since the glob pattern above is specific enough.
         let starting_index = path.find("next/dist").unwrap();
@@ -270,11 +270,11 @@ impl NextNodeSharedRuntimeResolvePlugin {
 #[turbo_tasks::value_impl]
 impl AfterResolvePlugin for NextNodeSharedRuntimeResolvePlugin {
     #[turbo_tasks::function]
-    fn after_resolve_condition(&self) -> Vc<AfterResolvePluginCondition> {
-        AfterResolvePluginCondition::new(
-            self.root.root(),
+    async fn after_resolve_condition(&self) -> Result<Vc<AfterResolvePluginCondition>> {
+        Ok(AfterResolvePluginCondition::new(
+            (*self.root.root().await?).clone(),
             Glob::new("**/next/dist/**/*.shared-runtime.js".into()),
-        )
+        ))
     }
 
     #[turbo_tasks::function]
@@ -285,7 +285,7 @@ impl AfterResolvePlugin for NextNodeSharedRuntimeResolvePlugin {
         _reference_type: Value<ReferenceType>,
         _request: Vc<Request>,
     ) -> Result<Vc<ResolveResultOption>> {
-        let stem = fs_path.file_stem().await?;
+        let stem = fs_path.file_stem();
         let stem = stem.as_deref().unwrap_or_default();
         let stem = stem.replace(".shared-runtime", "");
 
@@ -300,7 +300,7 @@ impl AfterResolvePlugin for NextNodeSharedRuntimeResolvePlugin {
             stem
         );
 
-        let raw_fs_path = &*fs_path.await?;
+        let raw_fs_path = fs_path.clone();
         let path = raw_fs_path.path.to_string();
 
         // Find the starting index of 'next/dist' and slice from that point. It should
@@ -311,7 +311,8 @@ impl AfterResolvePlugin for NextNodeSharedRuntimeResolvePlugin {
 
         let new_path = fs_path
             .root()
-            .join(format!("{base}/{resource_request}").into());
+            .await?
+            .join(format!("{base}/{resource_request}").into())?;
 
         Ok(Vc::cell(Some(ResolveResult::source(ResolvedVc::upcast(
             FileSource::new(new_path).to_resolved().await?,
@@ -394,11 +395,11 @@ impl NextSharedRuntimeResolvePlugin {
 #[turbo_tasks::value_impl]
 impl AfterResolvePlugin for NextSharedRuntimeResolvePlugin {
     #[turbo_tasks::function]
-    fn after_resolve_condition(&self) -> Vc<AfterResolvePluginCondition> {
-        AfterResolvePluginCondition::new(
-            self.root.root(),
+    async fn after_resolve_condition(&self) -> Result<Vc<AfterResolvePluginCondition>> {
+        Ok(AfterResolvePluginCondition::new(
+            (*self.root.root().await?).clone(),
             Glob::new("**/next/dist/esm/**/*.shared-runtime.js".into()),
-        )
+        ))
     }
 
     #[turbo_tasks::function]
