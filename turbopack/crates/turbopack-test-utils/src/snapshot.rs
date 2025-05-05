3dcc3f73fc712106dfbaf7ba8df6c8a606c6d773
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 15:31:51 -0700

    value to string

--- turbopack/crates/turbopack-test-utils/src/snapshot.rs
+++ turbopack/crates/turbopack-test-utils/src/snapshot.rs
@@ -156,14 +156,14 @@ pub async fn diff(path: FileSystemPath, actual: Vc<AssetContent>) -> Result<()> 
 
 async fn get_contents(file: Vc<AssetContent>, path: FileSystemPath) -> Result<Option<String>> {
     Ok(
-        match &*file
-            .await
-            .context(format!("Unable to read AssetContent of {}", path))?
-        {
-            AssetContent::File(file) => match &*file
-                .await
-                .context(format!("Unable to read FileContent of {}", path))?
-            {
+        match &*file.await.context(format!(
+            "Unable to read AssetContent of {}",
+            path.value_to_string().await?
+        ))? {
+            AssetContent::File(file) => match &*file.await.context(format!(
+                "Unable to read FileContent of {}",
+                path.value_to_string().await?
+            ))? {
                 FileContent::NotFound => None,
                 FileContent::Content(expected) => {
                     let rope = expected.content();
