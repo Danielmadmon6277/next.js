92eae7634a2b9396ff2fd760ffe85fea082b1651
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 12:02:52 -0700

    turbopack

--- turbopack/crates/turbopack/benches/node_file_trace.rs
+++ turbopack/crates/turbopack/benches/node_file_trace.rs
@@ -76,7 +76,7 @@ fn bench_emit(b: &mut Bencher, bench_input: &BenchInput) {
         async move {
             let task = tt.spawn_once_task(async move {
                 let input_fs = DiskFileSystem::new("tests".into(), tests_root.clone(), vec![]);
-                let input = input_fs.root().join(input.clone());
+                let input = input_fs.root().await?.join(input.clone())?;
 
                 let input_dir = input.parent().parent();
                 let output_fs: Vc<NullFileSystem> = NullFileSystem.into();
@@ -115,12 +115,12 @@ fn bench_emit(b: &mut Bencher, bench_input: &BenchInput) {
                 let module = module_asset_context
                     .process(Vc::upcast(source), Value::new(ReferenceType::Undefined))
                     .module();
-                let rebased = RebasedAsset::new(Vc::upcast(module), input_dir, output_dir)
+                let rebased = RebasedAsset::new(Vc::upcast(module), input_dir, output_dir.clone())
                     .to_resolved()
                     .await?;
 
                 let emit_op =
-                    emit_with_completion_operation(ResolvedVc::upcast(rebased), output_dir);
+                    emit_with_completion_operation(ResolvedVc::upcast(rebased), output_dir.clone());
                 emit_op.read_strongly_consistent().await?;
                 apply_effects(emit_op).await?;
 
