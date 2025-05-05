205b048ca58e97f5f072e9f2b3b292b918cb271f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 12:05:50 -0700

    next-core

--- crates/next-core/src/next_shared/transforms/next_react_server_components.rs
+++ crates/next-core/src/next_shared/transforms/next_react_server_components.rs
@@ -89,10 +89,7 @@ impl CustomTransformer for NextJsReactServerComponents {
                 dynamic_io_enabled: self.dynamic_io_enabled,
                 use_cache_enabled: self.use_cache_enabled,
             }),
-            match &self.app_dir {
-                None => None,
-                Some(path) => Some(path.path.clone().into()),
-            },
+            self.app_dir.as_ref().map(|path| path.path.clone().into()),
         );
 
         program.visit_with(&mut visitor);
