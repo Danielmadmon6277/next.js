f1f214e3d493e408a3b08defb50b4dc47f01aa4a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 10:09:20 -0700

    next-core

--- crates/next-core/src/embed_js.rs
+++ crates/next-core/src/embed_js.rs
@@ -17,7 +17,7 @@ pub(crate) fn next_js_file(path: RcStr) -> Vc<FileContent> {
 }
 
 #[turbo_tasks::function]
-pub(crate) fn next_js_file_path(path: RcStr) -> FileSystemPath {
+pub(crate) fn next_js_file_path(path: RcStr) -> Vc<FileSystemPath> {
     next_js_fs().root().join(path)
 }
 
