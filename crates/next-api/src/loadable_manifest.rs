d4d310658c5e9450d358106e5540b3ad4722ae8d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 12:47:24 -0700

    next-api

--- crates/next-api/src/loadable_manifest.rs
+++ crates/next-api/src/loadable_manifest.rs
@@ -29,7 +29,7 @@ pub async fn create_react_loadable_manifest(
 
         let id = module_id.to_string().owned().await?;
 
-        let client_relative_path_value = client_relative_path.await?;
+        let client_relative_path_value = client_relative_path.clone();
         let files = chunk_output
             .iter()
             .map(move |&file| {
