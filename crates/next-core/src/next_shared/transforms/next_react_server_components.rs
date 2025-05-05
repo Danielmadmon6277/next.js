b24e43952e6ab92da4ad3ed525d5c4821f89e2f4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-05 11:50:41 -0700

    next-core

--- crates/next-core/src/next_shared/transforms/next_react_server_components.rs
+++ crates/next-core/src/next_shared/transforms/next_react_server_components.rs
@@ -89,7 +89,7 @@ impl CustomTransformer for NextJsReactServerComponents {
                 dynamic_io_enabled: self.dynamic_io_enabled,
                 use_cache_enabled: self.use_cache_enabled,
             }),
-            match self.app_dir {
+            match &self.app_dir {
                 None => None,
                 Some(path) => Some(path.path.clone().into()),
             },
